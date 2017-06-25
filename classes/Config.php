25b77e58298dfa1fb9816388fde5e5c1383a08d6
weij <g23988@gmail.com>
2017-06-25 08:10:59 +0000

    fix error php7 to php5

--- classes/Config.php
+++ classes/Config.php
@@ -24,55 +24,55 @@ class Config{
         }
         //system setting
         function Replytime($Replytime=null){
-                $this->_Replytime = $Replytime ?? $this->_Replytime;
+                $this->_Replytime = isset($Replytime) ? $Replytime : $this->_Replytime;
                 return $this->_Replytime;
         }
         //line bot setting
         function Channel_ID($channel_ID=null){
-                $this->_Channel_ID = $channel_ID ?? $this->_Channel_ID;
+                $this->_Channel_ID = isset($channel_ID) ? $channel_ID : $this->_Channel_ID;
                 return $this->_Channel_ID;
         }
         function Channel_Secret($channel_Secret=null){
-                $this->_Channel_Secret = $channel_Secret ?? $this->_Channel_Secret;
+                $this->_Channel_Secret = isset($channel_Secret) ? $channel_Secret : $this->_Channel_Secret;
                 return $this->_Channel_ID;
         }
         function Channel_Token($channel_Token=null){
-                $this->_Channel_Token = $channel_Token ?? $this->_Channel_Token;
+                $this->_Channel_Token = isset($channel_Token) ? $channel_Token : $this->_Channel_Token;
                 return $this->_Channel_Token;
         }
         //redis setting
         function Redis_Server($redis_Server=null){
-                $this->_Redis_Server = $redis_Server ?? $this->_Redis_Server;
+                $this->_Redis_Server = isset($redis_Server) ? $redis_Server : $this->_Redis_Server;
                 return $this->_Redis_Server;
         }
         function Redis_Port($redis_Port=null){
-                $this->_Redis_Port = $redis_Port ?? $this->_Redis_Port;
+                $this->_Redis_Port = isset($redis_Port) ? $redis_Port : $this->_Redis_Port;
                 return $this->_Redis_Port;
         }
         function Redis_Timeout($redis_Timeout=null){
-                $this->_Redis_Timeout = $redis_Timeout ?? $this->_Redis_Timeout;
+                $this->_Redis_Timeout = isset($redis_Timeout) ? $redis_Timeout : $this->_Redis_Timeout;
                 return $this->_Redis_Timeout;
         }
         //elastic search
         function Elk_Server($elk_Server=null){
-                $this->_Elk_Server = $elk_Server ?? $this->_Elk_Server;
+                $this->_Elk_Server = isset($elk_Server) ? $elk_Server : $this->_Elk_Server;
                 return $this->_Elk_Server;
         }
         function Elk_Port($elk_Port=null){
-                $this->_Elk_Port = $elk_Port ?? $this->_Elk_Port;
+                $this->_Elk_Port = isset($elk_Port) ? $elk_Port : $this->_Elk_Port;
                 return $this->_Elk_Port;
         }
         function Elk_Index($elk_Index=null){
-                $this->_Elk_Index = $elk_Index ?? $this->_Elk_Index;
+                $this->_Elk_Index = isset($elk_Index) ? $elk_Index : $this->_Elk_Index;
                 return $this->_Elk_Index;
         }
         function Elk_Type($elk_Type=null){
-                $this->_Elk_Type = $elk_Type ?? $this->_Elk_Type;
+                $this->_Elk_Type = isset($elk_Type) ? $elk_Type : $this->_Elk_Type;
                 return $this->_Elk_Type;
         }
 	//aws sqs
 	function SQS_QueueUrl($sqs_QueueUrl=null){
-		$this->_SQS_QueueUrl = $sqs_QueueUrl ?? $this->_SQS_QueueUrl;
+		$this->_SQS_QueueUrl = isset($sqs_QueueUrl) ? $sqs_QueueUrl : $this->_SQS_QueueUrl;
 		return $this->_SQS_QueueUrl;
 	}
 
